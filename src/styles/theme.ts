88d0deadf5280376b33403740b2da848059b8adc
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-11 18:15:13 +0200

    :wrench: added theme config

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme, Theme } from '@chakra-ui/react';
+
+export const theme = extendTheme<Theme>({
+  colors: {
+    gray: {
+      '50': '#EEEEF2',
+      '100': '#D1D2DC',
+      '200': '#B3B5C6',
+      '300': '#9699B0',
+      '400': '#797D9A',
+      '500': '#616480',
+      '600': '#4B4B63',
+      '700': '#353646',
+      '800': '#1F2029',
+      '900': '#181B23',
+    },
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50',
+      },
+    },
+  },
+});
