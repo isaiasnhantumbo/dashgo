05f9e014dbc3a831f2c87eb0185768b869a921cb
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-12 15:12:58 +0200

    :recycle: refactor: splined Pagination in components

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,24 @@
+import { Box, HStack, Stack } from '@chakra-ui/react';
+import { PaginationItem } from './PaginationItem';
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <HStack spacing="2">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+      </HStack>
+    </Stack>
+  );
+}
